7dfab2ee71b3f761cc36effa84041da6b219e2ba
bemilbandin-ops <bemilbandin@gmail.com>
2026-07-08 20:24:46 +0200

    chore: configure github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/led-juggling-tracker/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
